381f11fda97827025e6994dfda9fc1a12b18bd62
cepuchino <niyaz1998@yandex.ru>
2023-10-10 12:49:39 +0300

    build update

--- rbi-pwa/scripts/indexed_db.js
+++ rbi-pwa/scripts/indexed_db.js
@@ -3,52 +3,51 @@ var db;
 var request = window.indexedDB.open("images_database", 1);
 
 request.onerror = function(event) {
-    console.log("IndexedDB access error: ", event.target.error);
+  console.log("Why didn't you allow my web app to use IndexedDB?!");
+};
+request.onsuccess = function(event) {
+  db = request.result;
 };
 
-request.onsuccess = function (event) {
-    db = request.result;
+request.onupgradeneeded = function(event) {
+  var db = event.target.result;
+  var objectStore = db.createObjectStore("images", { keyPath: "id" });
 };
 
-request.onupgradeneeded = function (event) {
-    var db = event.target.result;
-    var objectStore = db.createObjectStore("images", {keyPath: "id"});
-};
+window.saveImage = function(id, data) {
+  var transaction = db.transaction(["images"], "readwrite");
+  var objectStore = transaction.objectStore("images");
 
-window.saveImage = function (id, data) {
-    var transaction = db.transaction(["images"], "readwrite");
+  var blob = new Blob([new Uint8Array(data)], {type: "image/jpeg"}); // Adjust type accordingly
+  var request = objectStore.put({id: id, image: blob, timestamp: Date.now()});
+
+  request.onsuccess = function(event) {
+    console.log('Image saved successfully');
+  };
+}
+
+window.getImage = function(id, callback) {
+  try {
+    var transaction = db.transaction(["images"]);
     var objectStore = transaction.objectStore("images");
+    var request = objectStore.get(id);
 
-    var blob = new Blob([new Uint8Array(data)], {type: "image/jpeg"});
-    var request = objectStore.put({id: id, image: blob, timestamp: Date.now()});
-
-    request.onsuccess = function (event) {
-        console.log('Image saved successfully');
-    };
-}
-
-window.getImage = function (id, callback) {
-    try {
-        var transaction = db.transaction(["images"]);
-        var objectStore = transaction.objectStore("images");
-        var request = objectStore.get(id);
-
-        request.onsuccess = function (event) {
-            try {
-                var reader = new FileReader();
-                reader.onload = function (e) {
-                    callback(null, {image: reader.result, timestamp: request.result.timestamp});
-                };
-                reader.readAsArrayBuffer(request.result.image);
-            } catch (err) {
-                callback(err, null);
-            }
+    request.onsuccess = function(event) {
+      try {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+          callback(null, {image: reader.result, timestamp: request.result.timestamp});
         };
-    } catch (err) {
+        reader.readAsArrayBuffer(request.result.image);
+      } catch (err) {
         callback(err, null);
-    }
+      }
+    };
+  } catch (err) {
+    callback(err, null);
+  }
 }
 
-window.arrayBufferToList = function (buffer) {
-    return Array.from(new Uint8Array(buffer));
+window.arrayBufferToList = function(buffer) {
+  return Array.from(new Uint8Array(buffer));
 }
