2ed7c9d1ecbd3197c034b21d6454a5b7d475e1f5
cepuchino <niyaz1998@yandex.ru>
2023-11-26 14:37:47 +0300

    keyboard visibility change

--- rbi-pwa/flutter_service_worker.js
+++ rbi-pwa/flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "53ad7b1bbf223064f5f9b9ead39a5532",
-"index.html": "27fc3f22076eb937ece310a14ec63562",
+"index.html": "0dee4fae53579de4aa83d4cde5850eae",
 "/": "2d2ffe9a6213551374939fe6100ee042",
 "firebase-messaging-sw.js": "26d788a7073e562493a856202cf6ef46",
-"main.dart.js": "f01c5c2831a751d20162e8068a5496f4",
+"main.dart.js": "6ffb9c44c4a671a6c376b313c9633168",
 "r/index.html": "e4286056c94e02e9d7970b248145ba31",
 "r/fallback_deeplink/index.html": "208f5c97742a33ac6c2813c49c981847",
 "r/portfolioAnalytics/index.html": "8554d56089f5c429e8a573a8b5ed1e86",
