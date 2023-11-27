d4b4b752a8298b8344b73894d67b8a6811506186
cepuchino <niyaz1998@yandex.ru>
2023-11-27 14:13:48 +0300

    auto show keyboard on search page

--- rbi-pwa/flutter_service_worker.js
+++ rbi-pwa/flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "53ad7b1bbf223064f5f9b9ead39a5532",
-"index.html": "fb857b5377bbe30c7c7061f3d0883e8f",
+"index.html": "7b452183a57e2a49f28a650a658fe19c",
 "/": "2d2ffe9a6213551374939fe6100ee042",
 "firebase-messaging-sw.js": "26d788a7073e562493a856202cf6ef46",
-"main.dart.js": "6215013eba94db139b49e621a641c61f",
+"main.dart.js": "6139103cdd01a9c1907b858651992ec1",
 "r/index.html": "e4286056c94e02e9d7970b248145ba31",
 "r/fallback_deeplink/index.html": "208f5c97742a33ac6c2813c49c981847",
 "r/portfolioAnalytics/index.html": "8554d56089f5c429e8a573a8b5ed1e86",
