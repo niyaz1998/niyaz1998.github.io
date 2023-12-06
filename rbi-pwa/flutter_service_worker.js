b5a3dff6dda236272349d9d61548a400ae73ca69
cepuchino <niyaz1998@yandex.ru>
2023-12-06 14:54:25 +0300

    new web keyboard fix

--- rbi-pwa/flutter_service_worker.js
+++ rbi-pwa/flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "53ad7b1bbf223064f5f9b9ead39a5532",
-"index.html": "7f7c5fd75fb52d6413e93af12dfcf6a8",
+"index.html": "3ed158c3c094463b092199c8dca16f1b",
 "/": "91544ef3db6c8f8c9ef7d86bf7b17c8e",
 "firebase-messaging-sw.js": "26d788a7073e562493a856202cf6ef46",
-"main.dart.js": "653b0b4e9b92486fbc9b6ec0f1cfb361",
+"main.dart.js": "55080ba7f58f647dd1e8ba2157bee59a",
 "r/index.html": "04410cccb3b980a10effae7de95557ef",
 "r/fallback_deeplink/index.html": "208f5c97742a33ac6c2813c49c981847",
 "r/portfolioAnalytics/index.html": "f1e0bb98ff6fe8dcbab1cf38aafac68d",
