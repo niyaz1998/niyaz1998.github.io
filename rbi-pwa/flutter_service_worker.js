f93e47144a67b37f7bec9bfa913dcfdee9c5ef4e
cepuchino <niyaz1998@yandex.ru>
2023-12-08 08:26:13 +0300

    new build with new keyboard mechanism

--- rbi-pwa/flutter_service_worker.js
+++ rbi-pwa/flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "53ad7b1bbf223064f5f9b9ead39a5532",
-"index.html": "1acd729e8493107342e66cf0fb0131b0",
+"index.html": "ca07a661dddb381000e84802b3b7a931",
 "/": "5175bba99c4fe97e341616538585ef46",
 "firebase-messaging-sw.js": "26d788a7073e562493a856202cf6ef46",
-"main.dart.js": "b11caba5228570a6ec89c0ee9c222c19",
+"main.dart.js": "3b87cd63783ba38e1c270dbe6f7a9a0a",
 "r/index.html": "e4286056c94e02e9d7970b248145ba31",
 "r/fallback_deeplink/index.html": "208f5c97742a33ac6c2813c49c981847",
 "r/portfolioAnalytics/index.html": "c0337f3b2040852d1a13bb72d5d4e495",
