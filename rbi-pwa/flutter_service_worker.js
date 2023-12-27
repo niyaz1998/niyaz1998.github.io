01ebc077f519b3e59f67264aa2640969cb955dfc
cepuchino <niyaz1998@yandex.ru>
2023-12-27 11:29:03 +0300

    new rb link

--- rbi-pwa/flutter_service_worker.js
+++ rbi-pwa/flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "53ad7b1bbf223064f5f9b9ead39a5532",
-"index.html": "818adb9ee873e3954d332aa85020bc40",
+"index.html": "25f6adbe3b7dfa0f67dd41d2c08277db",
 "/": "5175bba99c4fe97e341616538585ef46",
 "firebase-messaging-sw.js": "03aee12d8e6333542d2e439666ff8d09",
-"main.dart.js": "ebbb8dd11667128db3d9138a62a1073a",
+"main.dart.js": "1b4203292c8fc89fa38dd75648995702",
 "r/index.html": "e4286056c94e02e9d7970b248145ba31",
 "r/fallback_deeplink/index.html": "4e2eeefa3d87cfabca96da6327f54360",
 "r/portfolioAnalytics/index.html": "c0337f3b2040852d1a13bb72d5d4e495",
