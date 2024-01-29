535f50fa7eb65da90d55d46f686eff163fac0209
cepuchino <niyaz1998@yandex.ru>
2024-01-29 13:19:14 +0300

    timurs changes

--- rbi-pwa/flutter_service_worker.js
+++ rbi-pwa/flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "fe25fbc961718e44dad08633102120bc",
-"index.html": "ea867d871e55efaf2a9be1d56ba03c11",
+"index.html": "8e5cd192157c2dc52cbf5593cdb75df5",
 "/": "5175bba99c4fe97e341616538585ef46",
 "firebase-messaging-sw.js": "03aee12d8e6333542d2e439666ff8d09",
-"main.dart.js": "bbee70f61a4177abcc4f34c5c63edb50",
+"main.dart.js": "a06310543b002378a9fbb4575552bcf5",
 "r/index.html": "e4286056c94e02e9d7970b248145ba31",
 "r/fallback_deeplink/rbi_fallback_redirect.html": "fedaf9179bd46d61565a3fe7ec776002",
 "r/portfolioAnalytics/index.html": "c0337f3b2040852d1a13bb72d5d4e495",
