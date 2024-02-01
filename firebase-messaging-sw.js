56f1961ee7c176027035eebb45c22500fd29c818
cepuchino <niyaz1998@yandex.ru>
2024-02-01 09:59:04 +0300

    falcone push notifications

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,14 +1,16 @@
 importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-messaging-compat.js");
 
+// this file must be at the root of the hosting
+
 const firebaseConfig = {
   apiKey: "AIzaSyASUBSs9AS4CwEQ_6vI4nQyoTXL6fnUA3k",
   authDomain: "rb-invest-prod.firebaseapp.com",
   projectId: "rb-invest-prod",
   storageBucket: "rb-invest-prod.appspot.com",
   messagingSenderId: "85300523227",
-  appId: "1:85300523227:web:77fd192c4f80683802dcfe",
-  measurementId: "G-9EBEJJG942"
+  appId: "1:85300523227:web:bf85487db146172f02dcfe",
+  measurementId: "G-KDWQC13XK0"
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -18,7 +20,4 @@ const messaging = firebase.messaging();
 // Optional:
 messaging.onBackgroundMessage((m) => {
   console.log("onBackgroundMessage", m);
-});
-messaging.onMessage((m) => {
-  console.log("onMessage", m);
 });
